6b46d9dca8abfe0b4060079b4a29eec37912fa7e
Akash <akashch1808@gmail.com>
2025-05-30 21:56:43 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon("postgresql://neondb_owner:npg_eX3EnDoWdFv1@ep-young-art-a4w9bmtm-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require")
+const sql = neon("process.env.DATABASE_URL")
 const db = drizzle({ client: sql });
 export default db
